b4553d29ccf3e76d4b9e9a11171e30095de36f82
Leonel Machin Remis <alfalink2000@gmail.com>
2026-06-29 03:49:27 +0200

    Deploy: Framer Motion + 3D

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Hero from './components/Hero'
+import ValueProposition from './components/ValueProposition'
+import Services from './components/Services'
+import SocialProof from './components/SocialProof'
+import ServiceArea from './components/ServiceArea'
+import CTA from './components/CTA'
+import Footer from './components/Footer'
+
+export default function App() {
+  return (
+    <>
+      <Hero />
+      <ValueProposition />
+      <Services />
+      <SocialProof />
+      <ServiceArea />
+      <CTA />
+      <Footer />
+    </>
+  )
+}
